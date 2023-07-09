d4b42bc08638ae1fad1ac9598b554379decdd729
DikshitGiri <dikshitgiri42@gmail.com>
2023-07-09 10:55:04 +0545

    initialization

---
+++ FrontEnd/src/pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+
+  
+function About() {
+    return (
+        <div>
+           <Navbar/> 
+         <p>This is about page</p>   
+        </div>
+    );
+}
+
+export default About;
